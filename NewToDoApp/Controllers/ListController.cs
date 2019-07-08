cfc6e9c185809c799b66e2eead63b378b9178ee5
Billy <cheefeng_0723@hotmail.com>
2019-07-08 21:00:50 +0800

    my project

---
+++ NewToDoApp/Controllers/ListController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewToDoApp.Models;
+using Newtonsoft.Json;
+using System.Data.Entity.Core.Objects;
+
+namespace NewToDoApp.Controllers
+{
+    public class ListController : Controller
+    {
+        // GET: List
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetDoneList()
+        {
+            using (ListDetailDBEntities entities = new ListDetailDBEntities())
+            {
+
+                var query = entities.ListDetails
+                       .SqlQuery("Select * from ListDetails where status='1' order by LID desc")
+                       .ToList<ListDetail>();
+
+                return Json(query);
+            }
+        }
+
+        public JsonResult GetWaitingList()
+        {
+            using (ListDetailDBEntities entities = new ListDetailDBEntities())
+            {
+
+                var query = entities.ListDetails
+                       .SqlQuery("Select * from ListDetails where status='0' order by LID desc")
+                       .ToList<ListDetail>();
+
+                return Json(query);
+            }
+            
+        }
+
+        [HttpPost]
+        public JsonResult InsertList(ListDetail newlist)
+        {
+            using (ListDetailDBEntities entities = new ListDetailDBEntities())
+            {
+                entities.ListDetails.Add(newlist);
+                entities.SaveChanges();
+                
+            }
+
+            return Json(newlist);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateList(ListDetail newlist)
+        {
+            using (ListDetailDBEntities entities = new ListDetailDBEntities())
+            {
+                ListDetail updatedList = (from c in entities.ListDetails
+                                          where c.LID == newlist.LID
+                                          select c).FirstOrDefault();
+                updatedList.title = newlist.title;
+                
+                updatedList.date = newlist.date;
+                updatedList.date = newlist.date.ToString();
+               // DateTime newdate = Convert.ToDateTime(newlist.date);
+                //DateTime newdate = DateTime.Parse(newlist.date);
+                //updatedList.date = newdate.ToString("dddd, dd MMMM yyyy");
+               
+
+                entities.SaveChanges();
+            }
+
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteList(int LID)
+        {
+            using (ListDetailDBEntities entities = new ListDetailDBEntities())
+            {
+                ListDetail oldlist = (from c in entities.ListDetails
+                                      where c.LID == LID
+                                      select c).FirstOrDefault();
+                entities.ListDetails.Remove(oldlist);
+                entities.SaveChanges();
+            }
+            return new EmptyResult();
+        }
+
+        public ActionResult UpdateDone(int LID)
+        {
+            using (ListDetailDBEntities entities = new ListDetailDBEntities())
+            {
+
+                ListDetail donelist = (from c in entities.ListDetails
+                                      where c.LID == LID
+                                      select c).FirstOrDefault();
+
+                donelist.status = "1";
+                //var query = entities.ListDetails
+                      //.SqlQuery("Update ListDetails set status='1' where LID= '" + LID + "'")
+                     // .ToList<ListDetail>();
+
+                
+                entities.SaveChanges();
+            }
+            return new EmptyResult();
+
+        }
+
+        public ActionResult UpdateUndo(int LID)
+        {
+            using (ListDetailDBEntities entities = new ListDetailDBEntities())
+            {
+
+                ListDetail donelist = (from c in entities.ListDetails
+                                       where c.LID == LID
+                                       select c).FirstOrDefault();
+
+                donelist.status = "0";
+                //var query = entities.ListDetails
+                //.SqlQuery("Update ListDetails set status='1' where LID= '" + LID + "'")
+                // .ToList<ListDetail>();
+
+
+                entities.SaveChanges();
+            }
+            return new EmptyResult();
+
+        }
+    }
+}
---
+++ NewToDoApp/Scripts/myAngular.js
@@ -0,0 +1,194 @@
+﻿var app = angular.module('MyApp', [])
+app.controller('MyController', function ($scope, $http, $window) {
+    //Getting records from database.
+    var post = $http({
+        method: "POST",
+        url: "/List/GetWaitingList",
+        dataType: 'json',
+        headers: { "Content-Type": "application/json" }
+    });
+    post.success(function (data, status) {
+        //The received response is saved in Customers array.
+        $scope.ListDetail = data;
+    });
+
+    var donepost = $http({
+        method: "POST",
+        url: "/List/GetDoneList",
+        dataType: 'json',
+        headers: { "Content-Type": "application/json" }
+    });
+    donepost.success(function (data, status) {
+        //The received response is saved in Customers array.
+        $scope.donedata = data;
+    });
+
+    //Adding new record to database.
+    $scope.Add = function () {
+        if (typeof ($scope.title) == "undefined" || typeof ($scope.date) == "undefined") {
+            return;
+        }
+
+
+        var post = $http({
+            method: "POST",
+            url: "/List/InsertList",
+            data: "{title: '" + $scope.title + "', date: '" + $scope.date + "', status: '" + 0 + "' }",
+            dataType: 'json',
+            headers: { "Content-Type": "application/json" }
+        });
+        post.success(function (data, status) {
+            //The newly inserted record is inserted into the Customers array.
+            alert("Add Successfully!");
+
+            $scope.ListDetail.push(data)
+
+        });
+        $scope.title = "";
+        $scope.date = "";
+    };
+
+    //This variable is used to store the original values.
+
+
+
+    //Waiting List Edit
+    $scope.EditItem = {};
+    //Editing an existing record.
+    $scope.Edit = function (index) {
+        //Setting EditMode to TRUE makes the TextBoxes visible for the row.
+        $scope.ListDetail[index].EditMode = true;
+
+        //The original values are saved in the variable to handle Cancel case.
+        $scope.EditItem.title = $scope.ListDetail[index].title;
+        $scope.EditItem.date = new Date($scope.ListDetail[index].date);
+        //alert(newDate);
+        //var FormatDate = newDate.toISOString().slice(0, 19).replace('T', ' ');
+        //$scope.EditItem.date = FormatDate;
+
+    };
+
+    //Cancelling an Edit.
+    $scope.Cancel = function (index) {
+        // The original values are restored back into the Customers Array.
+        $scope.ListDetail[index].title = $scope.EditItem.title;
+        $scope.ListDetail[index].date = $scope.EditItem.date;
+
+        //Setting EditMode to FALSE hides the TextBoxes for the row.
+        $scope.ListDetail[index].EditMode = false;
+        $scope.EditItem = {};
+    };
+
+    //Updating an existing record to database.
+    $scope.Update = function (index) {
+        var newlist = $scope.ListDetail[index];
+        var post = $http({
+            method: "POST",
+            url: "/List/UpdateList",
+            data: '{newlist:' + JSON.stringify(newlist) + '}',
+            dataType: 'json',
+            headers: { "Content-Type": "application/json" }
+        });
+        post.success(function (data, status) {
+            //Setting EditMode to FALSE hides the TextBoxes for the row.
+            newlist.EditMode = false;
+        });
+    };
+
+    //Done List Edit
+    $scope.EditItem = {};
+    //Editing an existing record.
+    $scope.DoneEdit = function (index) {
+        //Setting EditMode to TRUE makes the TextBoxes visible for the row.
+        $scope.donedata[index].EditMode = true;
+
+        //The original values are saved in the variable to handle Cancel case.
+        $scope.EditItem.title = $scope.donedata[index].title;
+        $scope.EditItem.date = $scope.donedata[index].date;
+    };
+
+    //Cancelling an Edit.
+    $scope.DoneCancel = function (index) {
+        // The original values are restored back into the Customers Array.
+        $scope.donedata[index].title = $scope.EditItem.title;
+        $scope.donedata[index].date = $scope.EditItem.date;
+
+        //Setting EditMode to FALSE hides the TextBoxes for the row.
+        $scope.donedata[index].EditMode = false;
+        $scope.EditItem = {};
+    };
+
+    //Updating an existing record to database.
+    $scope.DoneUpdate = function (index) {
+        var newlist = $scope.donedata[index];
+        var post = $http({
+            method: "POST",
+            url: "/List/UpdateList",
+            data: '{newlist:' + JSON.stringify(newlist) + '}',
+            dataType: 'json',
+            headers: { "Content-Type": "application/json" }
+        });
+        post.success(function (data, status) {
+            //Setting EditMode to FALSE hides the TextBoxes for the row.
+            newlist.EditMode = false;
+        });
+    };
+
+
+    //Deleting an existing record from database.
+    $scope.Delete = function (LID) {
+        if ($window.confirm("Do you want to delete this row?")) {
+            var post = $http({
+                method: "POST",
+                url: "/List/DeleteList",
+                data: "{LID: " + LID + "}",
+                dataType: 'json',
+                headers: { "Content-Type": "application/json" }
+            });
+            post.success(function (data, status) {
+
+                //Remove the Deleted record from the Customers Array.
+                $scope.ListDetail = $scope.ListDetail.filter(function (oldlist) {
+                    return oldlist.LID !== LID;
+                });
+                $scope.donedata = $scope.donedata.filter(function (oldlist) {
+                    return oldlist.LID !== LID;
+                });
+            });
+        }
+    };
+    //Update Done
+    $scope.Done = function (LID) {
+        var post = $http({
+            method: "POST",
+            url: "/List/UpdateDone",
+            data: "{LID: " + LID + "}",
+            dataType: 'json',
+            headers: { "Content-Type": "application/json" }
+        });
+        post.success(function (data, status) {
+
+            alert("Done Your Task!");
+            window.location.reload();
+        });
+
+    };
+    //Update Undo
+    $scope.Undo = function (LID) {
+        var post = $http({
+            method: "POST",
+            url: "/List/UpdateUndo",
+            data: "{LID: " + LID + "}",
+            dataType: 'json',
+            headers: { "Content-Type": "application/json" }
+        });
+        post.success(function (data, status) {
+            alert("Undo Your Task!");
+            window.location.reload();
+        });
+
+    };
+
+
+
+});
